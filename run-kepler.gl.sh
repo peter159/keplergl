0c016ce667ca1808760005d3d3eb35e4b19512b1
Tim Ganther <tim.ganther@telekom.de>
2020-10-08 11:28:15 +0200

    Delete run-kepler.gl.sh

--- run-kepler.gl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-docker run -p 8080:80 \
-	-d \
-	--name kepler.gl \
-	-e MapboxAccessToken="pk.eyJ1IjoibWFwYm95NCIsImEiOiJja2RmeTcwcjAxOHRmMnRxcTB6YXRrbTViIn0.-WOzwlBOcbkhXeSLdsYwVw" \
-	crazycapivara/kepler.gl
