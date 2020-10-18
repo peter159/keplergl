847af3bfd65d7de6158a9bbc9131bb854aa14549
Tim Ganther <tim.ganther@telekom.de>
2020-10-18 14:56:26 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 8080:80 -d --name keplergl -e MapboxAccessToken="pk.eyJ1IjoibWFwYm95NCIsImEiOiJja2RmeTcwcjAxOHRmMnRxcTB6YXRrbTViIn0.-WOzwlBOcbkhXeSLdsYwVw" serge4nt/keplergl
+docker run -p 8080:80 -d --name keplergl -e MapboxAccessToken="YOUR TOKEN GOES HERE" serge4nt/keplergl
